4a6022d12db5bea394b6323f9ccd8860f6231de7
Zerive05 <bgsman05@gmail.com>
2025-07-10 14:25:21 +0700

    14.25 10/07/2025

---
+++ database/migrations/2025_07_10_054858_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('userid');
+            $table->string('judul');
+            $table->string('gambar');
+            $table->string('konten');
+            $table->bigInteger('like');
+            $table->bigInteger('dislike');
+            $table->timestamps();
+
+            $table->foreign('userid')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
