4d5810625236932f9707c33f547d408b3aef0fac
Zerive05 <bgsman05@gmail.com>
2025-07-18 10:06:57 +0700

    10.06 18/07/2025

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,6 +16,25 @@ class DashboardController extends Controller
      *
      * @return \Inertia\Response
      */
+    public function adashboard()
+    {
+        $totalartikel = article::count();
+        $artikelpending = article::where('status', 'pending')->count();
+        $artikelthismonth = article::whereMonth('created_at', Carbon::now()->month)
+            ->whereYear('created_at', Carbon::now()->year)->count();
+        $totalMember = User::count();
+
+        return Inertia::render(
+            'DashboardAdmin',
+            [
+                'totalartikel' => $totalartikel,
+                'artikelpending' => $artikelpending,
+                'artikelthismonth' => $artikelthismonth,
+                'totalMember' => $totalMember
+            ]
+        );
+    }
+
     public function edashboard()
     {
         $totalartikel = article::count();
